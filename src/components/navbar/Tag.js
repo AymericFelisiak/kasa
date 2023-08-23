072acda0cc5b7b8d164438d787f0ff0c5837b65a
Aymeric Felisiak <aymeric.felisiak@gmail.com>
2023-08-23 12:27:10 +0200

    Added tag component + style

---
+++ src/components/navbar/Tag.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function Tag(props) {
+	return (
+		<div class="tag">
+			<p>{props.name}</p>
+		</div>
+	);
+}
--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -1,9 +1,11 @@
 import React from 'react'
+import Tag from '../../components/navbar/Tag'
 
 export default function Home() {
     return (
         <div>
             <h1>Home</h1>
+            <Tag name="Test"/>
         </div>
     )
 }
