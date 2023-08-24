0100877f30989f82ca2db21f7fa9c00303a676b4
Aymeric Felisiak <aymeric.felisiak@gmail.com>
2023-08-24 09:20:14 +0200

    Added base component for thumbnail + styling

---
+++ src/components/navbar/Thumbnail.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+export default function Thumbnail() {
+	return (
+		<div class="thumbnail">
+			<div class="title">
+                <h2>Titre de la location</h2>
+            </div>
+		</div>
+	);
+}
--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -1,11 +1,13 @@
 import React from 'react'
 import Tag from '../../components/navbar/Tag'
+import Thumbnail from '../../components/navbar/Thumbnail'
 
 export default function Home() {
     return (
         <div>
             <h1>Home</h1>
             <Tag name="Test"/>
+            <Thumbnail/>
         </div>
     )
 }
